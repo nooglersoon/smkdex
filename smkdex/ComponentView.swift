32ecccdb8e1e0d50d8e6a19e072b13b99ad6dd07
Fauzi Achmad Bangsa Diria <nooglersoon@outlook.co.id>
2021-07-14 18:03:00 +0700

    Menambahkan component view

--- smkdex/ComponentView.swift
+++ smkdex/ComponentView.swift
@@ -23,7 +23,7 @@ struct ComponentView: View {
                         .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 }
                 
-                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ")
                     
                     .multilineTextAlignment(.center)
                     .font(.callout)
