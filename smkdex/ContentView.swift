8c2f832d1a99789cb5df1faf537488065935ef97
Fauzi Achmad Bangsa Diria <nooglersoon@outlook.co.id>
2021-07-14 15:21:47 +0700

    Menambahkan ARView

--- smkdex/ContentView.swift
+++ smkdex/ContentView.swift
@@ -14,6 +14,30 @@ struct ContentView : View {
     }
 }
 
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+struct ARViewContainer_Previews: PreviewProvider {
+    static var previews: some View {
+        ARViewContainer()
+    }
+}
 
 
 #if DEBUG
