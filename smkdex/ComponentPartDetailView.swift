ca97e06731b651d6b97833605b016c35dc9d540a
Muhammad Gilang Nursyahroni <mgilangns.2710@gmail.com>
2021-07-17 19:41:13 +0700

    Add Component Part Detail View

--- smkdex/ComponentPartDetailView.swift
+++ smkdex/ComponentPartDetailView.swift
@@ -9,30 +9,41 @@ import SwiftUI
 
 struct ComponentPartDetailView: View {
     var body: some View {
-                VStack {
-                    Text("Tittle")
-                        .font(.system(size: 30, weight: .heavy, design: .default))
-                    Image("testpic")
-                        .resizable()
-                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    Text("What it is")
-                        .offset(x: -145)
-                        .font(.system(size: 15, weight: .heavy, design: .default))
-                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, ")
-                        .padding()
-                    Text("What it's function in horn electical circuit")
-                        .offset(x: -25)
-                        .font(.system(size: 15, weight: .heavy, design: .default))
-                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, ")
-                        .padding()
-                    Text("How to Install it")
-                        .offset(x: -120)
-                        .font(.system(size: 15, weight: .heavy, design: .default))
-                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text")
-                        .padding()
-                }
+        
+        VStack(spacing: 15){
+            
+            VStack{
+                Text("Sandwich")
+                    .font(.title)
+                   //Spacer()
             }
-        }
+            VStack{
+                Image("testpic")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                Spacer()
+                
+            }
+            VStack(alignment: .leading){
+                Text("What it is")
+                    .font(.headline)
+                Text("in publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.")
+            }
+            VStack(alignment: .leading){
+                Text("What it's function")
+                    .font(.headline)
+                Text("in publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.")
+            }
+            VStack(alignment: .leading){
+                Text("How to install it")
+                    .font(.headline)
+                Text("in publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.")
+            }
+        }.padding()
+        
+    }
+}
 
 
 struct ComponentPartDetailView_Previews: PreviewProvider {
